1a7e317803b8a6d24a7079c2eeddcc14644708de
lvxubo <3268643992@qq.com>
2022-06-23 19:32:10 +0800

    python的课堂作业

---
+++ 单位互换.py
@@ -0,0 +1,4 @@
+Q = float(input("Please enter the number you want to convert："))
+# 请输入你要转换的数字：
+W = 2.54
+print(f"{Q}inch={Q*W}cm")
---
+++ 成绩转换.py
@@ -0,0 +1,12 @@
+a = int(input("Please enter your score:"))
+# 请输入你的的成绩
+if a >= 90: #这是一个判断,用来判断a的
+    print("A")
+elif a < 90 and a >=80:
+    print("B")
+elif a <80 and a >= 70:
+    print("C")
+elif a <70 and a >=60 :
+    print("D")
+elif a <= 60:
+    print("E")
